1b39ebf388a41f2f55dcb011f874d5a5152e68df
Deepanshu <sainideepanshu764@ce.du.ac.in>
2025-11-19 18:37:49 +0530

    Delete project_universal_add_sub.srcs/sources_1/new/universal.v

--- project_universal_add_sub.srcs/sources_1/new/universal.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-module universal(
-input a,b,cin,m, output cout,sum
-    );
-    assign sum = a ^ (b ^ m) ^ cin;
- assign cout = (((a ^ (b ^ m)) & cin)) | (((b ^ m) & a));
-endmodule
